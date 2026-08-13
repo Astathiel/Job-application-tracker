03b43508a0adb406c45b875e967fa2456080cd4c
Astathiel <leohaanpaa99@gmail.com>
2026-08-13 10:45:12 +0300

    Initial Windows Form App and its requirements

---
+++ JobApplicationTracker/JobApplicationTracker/DataManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+namespace JobApplicationTracker
+{
+    public static class DataManager
+    {
+        private static readonly string FilePath = "data.json";
+
+        public static void SaveApplications(List<JobApplication> applications)
+        {
+            string jsonString = JsonSerializer.Serialize(applications);
+            File.WriteAllText(FilePath, jsonString);
+        }
+
+        public static List<JobApplication> LoadApplications()
+        {
+            if (!File.Exists(FilePath))
+            {
+                return new List<JobApplication>();
+            }
+
+            string jsonString = File.ReadAllText(FilePath);
+            return JsonSerializer.Deserialize<List<JobApplication>>(jsonString) ?? new List<JobApplication>();
+        }
+    }
+}
---
+++ JobApplicationTracker/JobApplicationTracker/Form1.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace JobApplicationTracker
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            submit_btn = new Button();
+            SuspendLayout();
+            // 
+            // submit_btn
+            // 
+            submit_btn.Location = new Point(2, 404);
+            submit_btn.Name = "submit_btn";
+            submit_btn.Size = new Size(112, 34);
+            submit_btn.TabIndex = 0;
+            submit_btn.Text = "Submit";
+            submit_btn.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1258, 664);
+            Controls.Add(submit_btn);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button submit_btn;
+    }
+}
