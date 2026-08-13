89d93eaaa137d4008d13eaebcdc3d7e2a32446ed
Astathiel <leohaanpaa99@gmail.com>
2026-08-13 11:38:34 +0300

    #

--- JobApplicationTracker/JobApplicationTracker/obj/Release/net8.0-windows/JobApplicationTracker.AssemblyInfo.cs
+++ JobApplicationTracker/JobApplicationTracker/obj/Release/net8.0-windows/JobApplicationTracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JobApplicationTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03b43508a0adb406c45b875e967fa2456080cd4c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d2be7f8956403a31541284c2466d226e2032f7c")]
 [assembly: System.Reflection.AssemblyProductAttribute("JobApplicationTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JobApplicationTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
