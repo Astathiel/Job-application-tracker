24a9eb946aaf2f4d12337ffb69491121e2a24407
Astathiel <leohaanpaa99@gmail.com>
2026-08-14 08:36:10 +0300

    Created JobApplicationTracker container

---
+++ JobApplicationTracker/JobApplicationTracker/JobApplication.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobApplicationTracker
+{
+    public class JobApplication
+    {
+        public string CompanyName { get; set; }
+        public string JobTitle { get; set; }
+        public string Location { get; set; }
+        public string WorkModel { get; set; }
+        public DateTime ApplicationDate { get; set; }
+        public string Status { get; set; }
+    }
+}
